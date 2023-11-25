0015e8f7fbb976d6b112626020f0d567fac07b47
sasan <sasanqc@gmail.com>
2023-11-25 16:43:00 +0330

    nav link active item

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,19 +1,31 @@
+"use client";
 import Link from "next/link";
 import React from "react";
+import { usePathname } from "next/navigation";
+import classNames from "classnames";
 
 const Navbar = () => {
+  const path = usePathname();
+  const links = [
+    { lable: "Dashboard", path: "/" },
+    { lable: "Issues", path: "/issues" },
+  ];
+
   return (
     <nav>
-      <ul className=" bg-indigo-700 text-red-50 flex gap-5 p-8  top-0">
-        <li className="">
-          <Link href={"/"}>LOGO</Link>
-        </li>
-        <li className="">
-          <Link href={"/"}>Dashboard</Link>
-        </li>
-        <li className="">
-          <Link href={"/issues"}>Issues</Link>
-        </li>
+      <ul className=" bg-indigo-700 text-red-50 flex gap-5 p-8">
+        {links.map((link) => (
+          <li key={link.lable}>
+            <Link
+              href={link.path}
+              className={classNames({
+                "pb-1 border-b-[2px]": path === link.path,
+              })}
+            >
+              {link.lable}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
